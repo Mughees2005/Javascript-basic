84627b64856954ae60a539f3df67bc5a29f74981
Mughees <smughees78@gmail.com>
2026-01-20 16:51:44 +0500

    add conditions file

---
+++ conditions.js
@@ -0,0 +1,27 @@
+// Syntax
+// if (condition) {
+//   // code to execute if the condition is true
+// }
+
+// if else example
+let num = 49;
+if (num > 50) {
+    console.log("Pass")
+} else if (num == 50){
+    console.log("promote")
+} else {
+    console.log("Fail")
+}
+
+
+// switch(expression) {
+//   case x:
+//     // code block
+//     break;
+//   case y:
+//     // code block
+//     break;
+//   default:
+//     // code block
+// }
+
