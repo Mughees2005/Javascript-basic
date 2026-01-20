24e76acc0f4d3a1a57b68508a5e591a970a96739
Mughees <smughees78@gmail.com>
2026-01-20 16:41:58 +0500

    add variable file

--- js_variables.js
+++ js_variables.js
@@ -1,11 +1,13 @@
 // javascript variables
 
+// let can be change in future
 let x = 10;
 let y = 20;
 let z = x + y;
 
 console.log(z); 
 
+// const is fix value and cannot be change
 const name = "Mughees";
 console.log(name);
 
