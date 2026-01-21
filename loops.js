06409e29759f2bc4c445b8b445322cf698269a72
Mughees <smughees78@gmail.com>
2026-01-21 09:18:09 +0500

    add loop

---
+++ loops.js
@@ -0,0 +1,38 @@
+// for loop
+
+// for (i=0; i<5; i++){
+//     console.log(i)
+// }
+
+// // while loop
+// let i = 0;
+// while (i<3){
+//     console.log(i);
+//     i++
+// }
+
+
+// do while
+// do-while loop pehle block ko execute karta hai aur phir condition check karta hai. Yeh loop at least ek baar execute hota hai.
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 3);
+
+// break => jahan break ki condition true ho wahan loop khatam hojaiga
+// continue => jahan continue ki condition true hogi wahn loop us walue ko chor dega
+ 
+// for (i=0; i<=10; i++){
+//     console.log(i)
+//     if (i == 3){
+//         break;
+//     }
+// }
+
+for (i=0; i<=5; i++){
+    if (i ==3){
+        continue
+    }
+    console.log(i)
+}
