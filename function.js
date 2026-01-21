35a441ba19582272429bf274f744323236eaaa3a
Mughees <smughees78@gmail.com>
2026-01-21 09:35:08 +0500

    add functions

---
+++ function.js
@@ -0,0 +1,39 @@
+// Use case:  used to organize, reuse, and modularize code, their primary use cases revolve around improving code efficiency, readability, and functionality in various programming scenarios. 
+
+// Syntax
+// function functionName(parameters) {
+//     // code to execute
+//     return value;
+// }
+
+// function greet(name){
+//     return "Hello " + name;
+// }
+// console.log(greet("Mughees"))
+
+// Arrow Function 
+// const multiply = (a,b) => {
+//     return a * b;
+// }
+// console.log(multiply(3,2))
+
+// const sqaure = (num) => {
+//     return num * num;
+// }
+// console.log(sqaure(3))
+
+
+// No Parameters, No Return
+// function sayHello(){
+//     console.log("Hello");
+// }
+// sayHello()
+
+
+// With Parameters, No Return
+// function greetUser(name){
+//     console.log("Welcome", name)
+// }
+// greetUser("mughees")
+
+
